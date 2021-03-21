53bf32ac47844e932a2f195fe4f4aabb8bdf3c2b
n643064 <anon643064@protonmail.com>
2021-03-21 16:53:01 +0100

    First commit
    I am stuff

---
+++ src/main/java/n6430/harvest/Harvest.java
@@ -0,0 +1,30 @@
+package n6430.harvest;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.block.Block;
+import net.minecraft.block.CropBlock;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+
+public class Harvest {
+    public static void init() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.mouse.wasRightButtonClicked()) {
+                HitResult TargetHit = client.crosshairTarget;
+                if (TargetHit.getType() == HitResult.Type.BLOCK) {
+                    BlockHitResult tblock = (BlockHitResult) TargetHit;
+                    BlockPos block_pos = tblock.getBlockPos();
+                    Block block = client.world.getBlockState(block_pos).getBlock();
+                    if (block instanceof CropBlock) {
+                        CropBlock cblock = (CropBlock) block;
+                        if (cblock.isMature(client.world.getBlockState(block_pos))) {
+                            client.interactionManager.attackBlock(block_pos, Direction.UP);
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
---
+++ src/main/java/n6430/harvest/client/HarvestClient.java
@@ -0,0 +1,14 @@
+package n6430.harvest.client;
+
+import n6430.harvest.Harvest;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class HarvestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        Harvest.init();
+    }
+}
